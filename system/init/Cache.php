a877a587965a3b6d2ae376582d06ab69a940b742
Namide <damien.git@gmail.com>
2014-02-26 16:25:54 +0100

    Sequential structure to tree structure for cache

--- system/init/Cache.php
+++ system/init/Cache.php
@@ -11,8 +11,14 @@ class Cache
 		global $_SYSTEM_DIRECTORY;
         include_once $_SYSTEM_DIRECTORY.'helpers/Url.php';
 		
-		global $_CACHE_DIRECTORY;
-        $this->pageFile = $_CACHE_DIRECTORY.Url::getURICacheID().'.cache';
+		global $_CACHE_DIRECTORY;		
+		$this->pageFile = $_CACHE_DIRECTORY.Url::getURICacheID();
+		$path = explode( "/", $this->pageFile );
+		if ( count( explode( ".", array_pop($path) ) ) < 2 )
+		{
+			$this->pageFile .= '/index.html';
+		}
+		
     }
 	
 	public function isCached()
@@ -22,9 +28,8 @@ class Cache
 	
 	public function echoCache()
 	{
-		$file_extension = strtolower(substr(strrchr( str_replace ( '.cache', '', $this->pageFile ) ,"."),1));
+		$file_extension = strtolower(substr(strrchr( $this->pageFile ,"."),1));
 
-		
 		switch ($file_extension)
 		{
 			case "xml": header('Content-Type: application/xml;'); break;
@@ -73,15 +78,46 @@ allow from all
 		$pageContent = ob_get_contents();
 		ob_end_clean();
 
-		if ( $page->getCachable() ) file_put_contents( $this->pageFile, $pageContent );
-		echo $pageContent ;
+		if ( $page->getCachable() )
+		{
+			$this->writesCacheFile( $pageContent );
+		}
+		echo $pageContent;
+	}
+	
+	private function writesCacheFile( &$pageContent )
+	{
+		$path = explode("/", $this->pageFile);
+		$dir = '';
+		while ( count($path) > 1 )
+		{
+			$dir .= $path[0].'/';
+			if ( !file_exists($dir) )
+			{
+				mkdir( $dir, 0777 );
+			}
+			array_shift($path);
+		}
+
+		$file = $path[0];
+		if ( count(explode( ".", $file )) > 1 )
+		{
+			$file = $dir.$file;
+		}
+		else
+		{
+			$file = $dir.'index.html';
+		}
+
+		file_put_contents( $file, $pageContent );
+		//file_put_contents( $this->pageFile, $pageContent );
 	}
 	
 	public static function getNumPages( $cacheDirectory )
 	{
 		$dir = $cacheDirectory;
 		if ( substr($dir, -1, 1) === '/' ) $dir = substr($dir, 0, -1);
-		return self::getNumPageRecurs($dir);//count( glob($cacheDirectory.'*.cache') );
+		return self::getNumPageRecurs($dir);
 	}
 	
 	private static function getNumPageRecurs( $dir )
