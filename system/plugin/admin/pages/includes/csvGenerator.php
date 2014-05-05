586796422ea0619f447d5da62cab70214133e890
Namide <damien.git@gmail.com>
2014-05-05 09:57:31 +0200

    Fix plugin/admin generate CSV

--- system/plugin/admin/pages/includes/csvGenerator.php
+++ system/plugin/admin/pages/includes/csvGenerator.php
@@ -4,12 +4,11 @@ function generateCsv( $csvName )
 {
 	include_once _SYSTEM_DIRECTORY.'init/imports.php';
 	include_once _SYSTEM_DIRECTORY.'init/loadPages.php';
-	include_once _SYSTEM_DIRECTORY.'init/Cache.php';
+	include_once _SYSTEM_DIRECTORY.'helpers/Cache.php';
+	include_once _SYSTEM_DIRECTORY.'helpers/WriteUtil.php';
 	
-	
-	$cache = new Cache();
 	$empty = '';
-	$cache->writesCacheFile($empty, $csvName);
+	WriteUtil::writeFile($empty, $csvName);
 	
 	$i = 0;
 	$contentCsv = array();
@@ -20,22 +19,10 @@ function generateCsv( $csvName )
 	
 	foreach (PageList::getInstance()->getPagesByUrl() as $page)
 	{
-		//$contentCsv .= getCsvLineByPage( $page );
 		fputcsv($csv, getCsvLineByPage( $page ), ';', '"');
 	}
 	
 	fclose($csv);
-
-	/*$cache = new Cache();
-	$cache->writesCacheFile($contentCsv, $csvName);
-	
-	$contentCsv = 'type;!id;!lang;url;title;description;template;header;visible;cachable;tags;phpHeader;contents;requests;!vo'."\r\n";
-	foreach (PageList::getInstance()->getPagesByUrl() as $page)
-	{
-		$contentCsv .= getCsvLineByPage( $page );
-	}*/
-	
-	
 }
 
 function getCsvLineByPage( &$page )
@@ -158,99 +145,3 @@ function isAssoc($arr)
 {
     return array_keys($arr) !== range(0, count($arr) - 1);
 }
-
-/*
-function getCsvLineByPage( &$page )
-{
-	//type;!id;!lang;url;title;description;template;header;visible;cachable;tags;phpHeader;contents;requests;!vo
-	include_once _SYSTEM_DIRECTORY.'core/Page.php';
-	include_once _SYSTEM_DIRECTORY.'core/DynamicPage.php';
-	
-	$type = ($page instanceof DynamicPage) ? 'dynamic' : 'static';
-	
-	$output = writeField($type);
-	$output .= writeField($page->getId());
-	$output .= writeField($page->getLanguage());
-	$output .= writeField($page->getUrl());
-	$output .= writeField($page->getTitle());
-	$output .= writeField($page->getDescription());
-	$output .= writeField($page->getTemplate());
-	$output .= writeField($page->getHeader());
-	$output .= writeField($page->getVisible());
-	$output .= writeField($page->getCachable());
-	$output .= writeField($page->getTags());
-	$output .= writeField($page->getPhpHeader());
-	$output .= writeField($page->getContents());
-	$output .= writeField($page->getRequests());
-	
-	if ($type === 'dynamic')	$output .= writeField($page->getVo());
-	else						$output .= writeField('');
-	
-	return $output."\r\n";
-}
-
-function writeField( $content )
-{
-	if ( gettype ($content) == "array" || gettype($content) == "object" ) 
-	{
-		return '"'.generateConstructor($content).'";';
-	}
-	elseif ( gettype ($content) == "boolean"  )
-	{
-		return ($content)?'"true";':'"false";';
-	}
-	return '"'.str_replace('"', '""', $content).'";';
-}
-
-function generateConstructor( $data )
-{
-	//include_once _SYSTEM_DIRECTORY.'core/RequestPage.php';
-	
-	if ( gettype ($data) != "array" && gettype($data) != "object" ) 
-	{
-		return str_replace('"', '""', '"'.escQuot($data).'"');
-	}
-	
-	
-	$output = 'array(';
-	$first = TRUE;
-	foreach ($data as $key => $value)
-	{
-		if ( !$first ) $output .= ',';
-
-		$output .= '""'.$key.'""=>';
-		if ( gettype ($value) === "array" ) 
-		{
-			$output	.= generateConstructor($value);
-		}
-		else if ( gettype($value) === "object" )
-		{
-			if( get_class($value) === "RequestPage" )
-			{
-				$output .= $value->getSave();
-			}
-			else
-			{
-				$output .= generateConstructor( $value );
-			}
-		}
-		elseif( gettype ($value) === "string" )
-		{
-			$output	.= escQuot($value);
-		}
-		else
-		{
-			$output	.= '"'.$value.'"';
-		}
-
-		if ( $first ) $first = FALSE;
-	}
-	$output .= ')';
-	return $output;
-}
-
-function escQuot( $text )
-{
-	return '"' . str_replace('"', '\"', $text ) .'"';
-}
-*/
